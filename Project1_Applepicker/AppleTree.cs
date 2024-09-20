68d45f8567c31cb8bcaa2b2c10a0cf04a1bb237d
Sage Phillips <sage242@hotmail.com>
2024-09-20 16:56:58 -0500

    Added apple.

--- Project1_Applepicker/AppleTree.cs
+++ Project1_Applepicker/AppleTree.cs
@@ -15,12 +15,36 @@ public float chanceToChangeDirections = 0.1f;
 public float secondsBetweenAppleDrops = 1f;
 void Start () {
 // Dropping apples every second
+Invoke( "DropApple", 2f); // a
 }
+
+void DropApple() { // b
+GameObject apple = Instantiate<GameObject>( applePrefab ); // c
+apple.transform.position = transform.position; // d
+Invoke( "DropApple", secondsBetweenAppleDrops ); // e
+}
+
 void Update () {
 // Basic Movement
 Vector3 pos = transform.position; // b
 pos.x += speed * Time.deltaTime; // c
 transform.position = pos; // d
 // Changing Direction
+if ( pos.x < -leftAndRightEdge ) { // a
+speed = Mathf.Abs(speed); // Move right // b
+} else if ( pos.x > leftAndRightEdge ) { // c
+speed = -Mathf.Abs(speed); // Move left // c
+} else if (Random.value < chanceToChangeDirections ) { // a
+speed *= -1; // Change direction // b
+}
+}
+
+void FixedUpdate() {
+// Changing Direction Randomly is now time-based because of
+FixedUpdate()
+if ( Random.value < chanceToChangeDirections ) { //
+b
+speed *= -1; // Change direction
+}
 }
 }
