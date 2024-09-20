7d990c5996e1a8bf20f2ea6eed04a5efabbe1514
Sage Phillips <sage242@hotmail.com>
2024-09-20 16:59:15 -0500

    Added Basket.cs

--- Project1_Applepicker/Apple.cs
+++ Project1_Applepicker/Apple.cs
@@ -6,6 +6,11 @@ public static float bottomY = -20f; // a
 void Update () {
 if ( transform.position.y < bottomY ) {
 Destroy( this.gameObject ); // b
+// Get a reference to the ApplePicker component of Main Camera
+ApplePicker apScript = Camera.main.GetComponent<ApplePicker>
+(); // b
+// Call the public AppleDestroyed() method of apScript
+apScript.AppleDestroyed();
 }
 }
 }
---
+++ Project1_Applepicker/Basket.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Basket : MonoBehaviour {
+
+    [Header("Set Dynamically")]
+public Text scoreGT; // a
+void Start() {
+// Find a reference to the ScoreCounter GameObject
+GameObject scoreGO = GameObject.Find("ScoreCounter"); // b
+// Get the Text Component of that GameObject
+scoreGT = scoreGO.GetComponent<Text>(); // c
+// Set the starting number of points to 0
+scoreGT.text = "0";
+
+void Update () {
+// Get the current screen position of the mouse from Input
+Vector3 mousePos2D = Input.mousePosition; //
+a
+// The Camera's z position sets how far to push the mouse into 3D
+mousePos2D.z = -
+Camera.main.transform.position.z; // b
+// Convert the point from 2D screen space into 3D game world space
+Vector3 mousePos3D = Camera.main.ScreenToWorldPoint( mousePos2D ); // c
+// Move the x position of this Basket to the x position of the Mouse
+Vector3 pos = this.transform.position;
+pos.x = mousePos3D.x;
+this.transform.position = pos;
+}
+
+void OnCollisionEnter( Collision coll ) { // a
+// Find out what hit this basket
+GameObject collidedWith = coll.gameObject; // b
+if ( collidedWith.tag == "Apple" ) { // c
+Destroy( collidedWith );
+// Parse the text of the scoreGT into an int
+int score = int.Parse( scoreGT.text ); // d
+// Add points for catching the apple
+score += 100;
+// Convert the score back to a string and display it
+scoreGT.text = score.ToString();
+}
+}
+}
